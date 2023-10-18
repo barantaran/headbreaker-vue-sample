94053e634cf6d2bb460ae3a651430cc92f236439
Dmitri Yourchev <d.yurchev@yandex.ru>
2023-10-18 16:25:14 +0000

    Puzzle first approach

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,57 @@
 <template>
-  <div id="app">
-    <div>Headbreaker from Vue</div>
     <div id="puzzle"></div>
-  </div>
 </template>
 
 <script>
 import headbreaker from 'headbreaker';
+let appWidth = 900;
 
 const drawPuzzle = () => {
-  const autogen = new headbreaker.Canvas('puzzle', {
-    width: 800,
-    height: 650,
-    pieceSize: 100,
-    proximity: 20,
-    borderFill: 10,
-    strokeWidth: 2,
-    lineSoftness: 0.18,
-    painter: new headbreaker.painters.Konva(),
-  });
+  let puzzleImage = new Image();
+  puzzleImage.src = '/photorealistic_face_of_ancient_daemon.png';
+  puzzleImage.onload = () => {
 
-  autogen.autogenerate({
-    horizontalPiecesCount: 2,
-    verticalPiecesCount: 2,
-    metadata: [
-      { color: '#B83361' },
-      { color: '#B87D32' },
-      { color: '#A4C234' },
-      { color: '#37AB8C' },
-    ],
-  });
-
-  autogen.draw();
+    const autogen = new headbreaker.Canvas('puzzle', {
+      width: appWidth,
+      height: 900,
+      pieceSize: 150,
+      proximity: 20,
+      borderFill: 15,
+      strokeWidth: 5,
+      lineSoftness: 0.18,
+      painter: new headbreaker.painters.Konva(),
+      image: puzzleImage
+    });
+    
+    autogen.adjustImagesToPuzzleHeight();
+    autogen.autogenerate({
+      horizontalPiecesCount: 4,
+      verticalPiecesCount: 4
+    });
+    
+    autogen.shuffleGrid();
+    autogen.draw();
+  }
 };
 
 export default {
   mounted() {
     drawPuzzle()
-  },
+  }
 };
 </script>
+<style>
+#app
+{
+  width: 900px;
+  background-color: black;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  right: 0;
+  margin: auto;
+  transform: translateY(-50%);
+  border-radius: 50px;
+}
+</style>
